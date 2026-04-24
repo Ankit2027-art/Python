50ee012ed1bdc8dc7900c885980dccf333e687ea
Ankit Patel <ankit8400764325@gmail.com>
2026-04-24 06:30:20 +0000

    Escape oprations

---
+++ 01_basics/02_escape.py
@@ -0,0 +1,24 @@
+print("hey how are you\n how are you \\ wline")
+print("hello \ word")
+ 
+print("hello \n world!")
+print("this is a tab\t charector")
+
+
+print("hello ankit \n how are you")
+print("hello ji\thow are you")
+
+
+print("\n")
+print("Name: Ankit Patel \nPath: C:\\Users\\Ankit\\Documents\nQuote: \"Stay hungry, stay foolish.\"\nThis is line one.\nThis is line two.")
+
+
+print("Hello \nWorld \npython")
+print("Name:\tAnkit patel")
+print("Course:\tBCA")
+print("Year:\t2nd")
+print("He said,\"Python is easy!\" ")
+print("It's Ankit's notebook.")
+print("C:\\Users\\Ankit\\Documents\\Python")
+print("123456\rABCDE")
+print("Python\b")
