4c628524b4abe4e45b56f02528f81cd48babfcc0
Ankit Patel <ankit8400764325@gmail.com>
2026-05-16 16:42:25 +0000

    Input

---
+++ 01_basics/01_input.py
@@ -0,0 +1,16 @@
+# How to take input by user
+
+# a=input("enter a value of a:")
+a=int(input("enter a value of a:"))
+
+# a=int(a)
+b=input("enter value of b:")
+b=int(b)
+
+print(a+b)
+
+
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+
+print(c+d)
