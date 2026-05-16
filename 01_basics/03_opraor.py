e12c86bafe14d0a8eb9bb96159fe1ac7639fe24e
Ankit Patel <ankit8400764325@gmail.com>
2026-05-16 16:42:36 +0000

    Oprators

---
+++ 01_basics/03_opraor.py
@@ -0,0 +1,38 @@
+# 1. Arithmetic Operators
+a = 10
+b = 3
+print("Arithmetic Operators:")
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+
+# 2. Assignment Operators
+print("\nAssignment Operators:")
+x = 5
+x += 2
+print("x += 2 ->", x)
+x *= 3
+print("x *= 3 ->", x)
+
+# 3. Comparison Operators
+print("\nComparison Operators:")
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
+
+# 4. Logical Operators
+print("\nLogical Operators:")
+print("True and False:", True and False)
+print("True or False:", True or False)
+print("not True:", not True)
+
+# 5. Bitwise Operators
+print("\nBitwise Operators:")
+print
