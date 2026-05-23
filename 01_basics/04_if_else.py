a21156c69968a0f12e654349a9636085c78fdae1
Ankit Patel <ankit8400764325@gmail.com>
2026-05-23 11:39:56 +0000

    If_else

---
+++ 01_basics/04_if_else.py
@@ -0,0 +1,111 @@
+# “If-else is a decision-making statement in Python.??
+
+    # It is used to execute different blocks of code based on a condition.
+    # If the condition is true, the if block executes; otherwise, the else block executes.”
+
+
+'''age =int(input("enter your age:"))
+if(age>18):
+    print("you can drive")
+    print("thank you")
+elif(age == 18):
+    print("Lets schedule an interview")
+else:
+    print("you can't drive")
+    print("thank you")'''
+
+
+
+#1-even odd
+'''a=int(input("enter a numer:"))
+if(a%2==0):
+    print("number is even")
+else:
+    print("number is odd")'''
+
+print("\n")
+
+#2-largest of two number
+'''b=int(input("enter a first number:"))
+c=int(input("enter a second number"))
+if(b>c):
+    print("first num is greater then sec")
+else:
+    print("sec num is greater then first num")'''
+
+#3-age group cheaker
+'''age=int((input("enter a age:")))
+if(age<13):
+    print("child")
+elif(13<=age<20): #WE CAN USE TWO OPERATORS ASIDE
+    print("Teenager")
+else:
+    print("Adult")'''
+
+#4-postive negative zero cheak
+'''a=int(input("enter a number:"))
+if(a>0):
+    print("number is postive")
+elif(a==0):
+    print("number is zero")
+else:
+    print("number is negative")'''
+
+# pass or fail
+'''num=int(input("enter a number:"))
+if(num>=35):
+    print("passed")
+else:
+    print("failed")'''
+
+
+
+'''marks=int(input("Enter a marks of students:"))
+if marks>=90:
+    print("Ex")
+elif marks>=90:
+    print("A")
+elif marks>=80:
+    print("B")
+
+elif marks>=70:
+    print("C")
+elif marks>=60:
+    print("D")
+else:
+    print("F")'''
+
+
+'''a = int(input("enter the number a: "))
+b=int(input("enter the number b:"))
+
+if (a>b):
+    print("a is greater than b")
+
+elif(a==b):
+    print("a is equal to b")
+
+else:
+    print("b is greater than a")
+
+print("commmands end")'''
+
+
+'''ch=input("enter a charecter:")
+
+if ch in "AEIOUaeiou":
+    print("Char is vovel")
+else:
+    print("Char is consonent")'''
+
+# THIS IS FOR STRING INPUT NOT FOR INDIVIDUAL CHAR
+
+'''a=(input("enter the word:"))
+b="Ankit"
+if a==b:
+    print("you are elligible")
+
+else:
+    print("you are not")'''
+    
+
