d9dea393155c43692a33f648b2c527c08eeed2a5
Ankit Patel <ankit8400764325@gmail.com>
2026-05-24 02:12:40 +0000

    Match box

---
+++ 01_basics/05_match_box.py
@@ -0,0 +1,44 @@
+'''a=int(input("enter a number between 1 to 10:"))
+match a:
+    case 1: 
+        print("you won a charger")
+    case 3:
+        print("you won a $3")
+    case 6:
+        print("you won a camera")
+    case _:
+        print("better luck next time")'''
+
+  
+# TICKET PRICE IS 1000
+
+b=int(input("enter a  number between 1 to 20:"))
+match b:
+    case 1:
+        print("you won a mobile")
+    case 10:
+        print("you won a CAR")
+    case 19:
+        print("you won a magic box")
+    case 4:
+        print("you won a ac")
+    case 8:
+        print("you won a russian girl")
+    case 20:
+        print("woahhhh you get chance to meet virat kohli")
+    case 9:
+        print("trip to hawai")
+    case 12:
+        print("woaahhh you win lifetime dominos meal")
+    case 16:
+        print("you won a dildo")
+    
+    
+        
+
+    case 2:
+        print("saaaaat karoooddddddd")
+
+
+
+
