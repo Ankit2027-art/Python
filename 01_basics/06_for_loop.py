364465544702263040822a7cdc68e78100157dde
Ankit Patel <ankit8400764325@gmail.com>
2026-05-25 03:24:07 +0000

    For_loop

---
+++ 01_basics/06_for_loop.py
@@ -0,0 +1,47 @@
+# “A for loop in Python is used to repeat a block of code multiple times.
+# It is mainly used for iterating over a sequence like a list, string, or range of numbers.”
+
+
+'''for i in range(1,6): #RANGE FUNCTION GOES from 1 to 5
+    print(i  )
+
+
+ 
+#the range function goes fron first number to 2 nd number -1(2nd-1)
+
+for i in range(1,11):
+    print("5 X",i,"=",5*(i))
+
+print(("\n"))
+
+
+for i in range(1,11):
+    print("10 x",i,"=",10*(i))
+
+
+ 
+
+for i in range(1,11):
+    print("5*",i,"=",i*5)'''
+
+#Print num from 1 to 10
+'''for i in range(1,11):
+    print(i)'''
+#print even num between 1 and 50
+'''for i in range(1,51):
+    if i % 2 ==0:
+        print(i)'''
+   
+#muliplication of 5
+'''for i in range(1,11):
+    print("5 X",i,"=",i*5)'''
+
+#sum of num 1to 100
+
+'''total = 0
+for i in range(1, 101):
+    total += i
+
+print("Sum of numbers from 1 to 100 is:", total)'''
+
+
